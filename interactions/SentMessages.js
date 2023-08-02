078fb9d1c9ea5e5ff9acddc9acc27719dd13a385
Ckaeiront <stampedesw29@gmail.com>
2023-08-02 17:53:14 -0200

    gifs sentido

--- interactions/SentMessages.js
+++ interactions/SentMessages.js
@@ -6,6 +6,15 @@ const senecaQuotes = [
   "Religion is regarded by the common people as true, by the wise as false, and by rulers as useful.",
 ];
 
+const sentidoGifs = [
+  'https://tenor.com/view/attack-on-titan-season3-eren-mikasa-armin-gif-20214159',
+  'https://tenor.com/view/anime-vinland-saga-thorfinn-windy-gif-15052575',
+  'https://tenor.com/view/yoru-plxto-xhouls-vinto-discord-gif-10159847829461245415',
+  'https://tenor.com/view/anime-gif-23546954',
+  'https://tenor.com/view/goathinn-gif-25535156',
+  'https://tenor.com/view/thorfinn-thorfinn-my-beloved-my-beloved-my-beloved-meme-vinland-saga-gif-23575917',
+];
+
 let randomNum = function(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
@@ -16,34 +25,38 @@ const messageHandler = (message, msgObject) => {
       "vai na desse cara n, todo catolicozinho esse da o cu"
     );
   }
-  if (message.includes("tiozao") || message.includes("tio")) {
+  if (message.toLowerCase().includes("tiozao") || message.toLowerCase().includes("tio")) {
     if (message.includes('?')) {
-    msgObject.repy('ta lá no seaga yi');
+    msgObject.reply('ta lá no seaga yi');
     return;
   }
     msgObject.reply("esse veio não sabe quantas vezes já me salvou");
   }
-  if (message.includes("tata")) {
+  if (message.toLowerCase().includes("tata")) {
     msgObject.channel.send(
       "há mais de 4 mil anos antes de cristo, o filósofo indiano Tataha Buddha escreveu o livro Brahamahaj, o Portal da Consiência. Esse livro foi o principal responsável por popularizar a meditação no oriente do jeito que conhecemos hoje."
     );
   }
-  if (message.includes("seneca")) {
+  if (message.toLowerCase().includes("seneca")) {
     let num = randomNum(0, 4);
     msgObject.channel.send(senecaQuotes[num]);
   }
-  if (message.includes('chi')) {
-    if (message.includes('vou sair')) {
+  if (message.toLowerCase().includes('chi')) {
+    if (message.toLowerCase().includes('vou sair')) {
       msgObject.channel.send('tu n é nem doido de sair do seaga yi');
       return;
     }
     msgObject.channel.send('C.H.I. também conhecido como Clube do Homem Independente, é um grupo do Tio MGTOW que eu to lá tbm');
   }
-  if (message.includes('bruno uchoa')) {
+  if (message.toLowerCase().includes('bruno uchoa')) {
     msgObject.channel.send('ta falando daquele saradão barbudo gostosão que emagrece as rabudas? adoro ele');
   }
-  if (message.includes('geraldao')) {
+  if (message.toLowerCase().includes('geraldao')) {
     msgObject.channel.send('geraldão, a.k.a. João Guilherme Beltrão é um estudante de Sistemas de informação, famoso por falar: "eai caraaaa", "porra caraaaaa", "oi caraaaa"');
+  }
+  if (message.toLowerCase().includes('sentido')) {
+    let num = randomNum(0, sentidoGifs.length - 1);
+    msgObject.channel.send(sentidoGifs[num]);
   }
 };
 
